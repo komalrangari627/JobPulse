ee5a6d28d0001ec17c3b6c1e054dafdbeac46018
komal <komalrangari627@gmail.com>
2025-11-03 19:19:04 +0530

    password reset and login user

--- server/Routers/userRouter.js
+++ server/Routers/userRouter.js
@@ -1,10 +1,19 @@
 import express from "express";
-import {test, handleUserRegister, handleOTPVerification,} from "../controllers/userController.js";
+import {
+    test,
+    handleUserRegister,
+    handleOTPVerification,
+    handleUserLogin,
+    handleResetPasswordRequest,
+ handleOTPForPasswordReset
+} from "../controllers/userController.js";
 
 const router = express.Router();
 
 router.get("/test", test);
 router.post("/register", handleUserRegister);
 router.post("/verify-otp", handleOTPVerification);
-
+router.post("/user-login", handleUserLogin)
+router.post("/password-reset-request", handleResetPasswordRequest)
+router.post("/verify-reset-password-request", handleOTPForPasswordReset)
 export default router;
