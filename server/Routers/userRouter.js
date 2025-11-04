902bc771cd136190435fc2c626d7f128e3dddd62
komal <komalrangari627@gmail.com>
2025-11-04 09:51:42 +0530

    file handling

--- server/Routers/userRouter.js
+++ server/Routers/userRouter.js
@@ -5,15 +5,22 @@ import {
     handleOTPVerification,
     handleUserLogin,
     handleResetPasswordRequest,
- handleOTPForPasswordReset
+    handleOTPForPasswordReset,
+    handleUserFileUplaod
 } from "../controllers/userController.js";
+import { AuthUser } from "../middlewares/AuthUser.js";
+import { upload } from "../config/multerConfig.js";
 
 const router = express.Router();
 
 router.get("/test", test);
 router.post("/register", handleUserRegister);
 router.post("/verify-otp", handleOTPVerification);
-router.post("/user-login", handleUserLogin)
-router.post("/password-reset-request", handleResetPasswordRequest)
-router.post("/verify-reset-password-request", handleOTPForPasswordReset)
+router.post("/user-login", handleUserLogin);
+router.post("/password-reset-request", handleResetPasswordRequest);
+router.post("/verify-reset-password-request", handleOTPForPasswordReset);
+// to upload resume/profie/docs we need to verfiy the user
+
+router.post("/upload-file/:file_type", AuthUser, upload.single("file"), handleUserFileUplaod);
+
 export default router;
