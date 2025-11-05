2b8cbea50388eadb74f87467be0ce76b3a53e589
komal <komalrangari627@gmail.com>
2025-11-05 12:27:55 +0530

    company routes

--- server/Routers/userRouter.js
+++ server/Routers/userRouter.js
@@ -6,7 +6,7 @@ import {
     handleUserLogin,
     handleResetPasswordRequest,
     handleOTPForPasswordReset,
-    handleUserFileUplaod
+    handleUserFileUpload
 } from "../controllers/userController.js";
 import { AuthUser } from "../middlewares/AuthUser.js";
 import { upload } from "../config/multerConfig.js";
@@ -21,6 +21,6 @@ router.post("/password-reset-request", handleResetPasswordRequest);
 router.post("/verify-reset-password-request", handleOTPForPasswordReset);
 // to upload resume/profie/docs we need to verfiy the user
 
-router.post("/upload-file/:file_type", AuthUser, upload.single("file"), handleUserFileUplaod);
+router.post("/upload-file/:file_type", AuthUser, upload.single("file"), handleUserFileUpload);
 
 export default router;
