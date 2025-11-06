7d1e13467b74f9c98d399f8cd716711861d2bd3f
komal <komalrangari627@gmail.com>
2025-11-06 23:25:24 +0530

    companyRoutes

--- server/Routers/companyRouter.js
+++ server/Routers/companyRouter.js
@@ -1,17 +1,37 @@
 import express from "express";
-import { handleUserFileUpload } from "../controllers/userController.js";
-import { upload } from "../multerConfig.js";
-import { AuthUser } from "../middleware/AuthUser.js";
+import {
+  registerCompany,
+  verifyCompanyOtp,
+  loginCompany,
+  updateCompany,
+  deleteCompany,
+  handleCompanyFileUpload,
+  handleCompanyPasswordResetRequest,
+  handleCompanyOTPForPasswordReset,
+} from "../controllers/companyController.js";
+
+import { AuthUser } from "../middlewares/AuthUser.js";
+import { AuthUniversal } from "../middlewares/AuthUniversal.js";
+import { upload } from "../config/multerConfig.js";
 
 const router = express.Router();
 
+router.post("/register", upload.single("companyLogo"), registerCompany);
+router.post("/verify-otp", verifyCompanyOtp);
+router.post("/company-login", loginCompany);
+router.put("/update/:id", upload.single("companyLogo"), updateCompany);
+router.delete("/delete/:id", deleteCompany);
 
+//  Password Reset Routes
+router.post("/password-reset-request", handleCompanyPasswordResetRequest);
+router.post("/verify-reset-password-request", handleCompanyOTPForPasswordReset);
 
+// Works for both users & companies
 router.post(
   "/upload-file/:file_type",
-  AuthUser,                //  Keep JWT validation
+  AuthUniversal,
   upload.single("file"),
-  handleUserFileUpload
+  handleCompanyFileUpload
 );
 
 export default router;
