ca0a4b5944cb84024fc41a6345f7a045f2f987ec
komal <komalrangari627@gmail.com>
2025-11-16 20:37:35 +0530

    login-register-dashboard

--- client/src/api/userAPI.js
+++ client/src/api/userAPI.js
@@ -1,25 +1,21 @@
-import axios from "axios"
+import axios from "axios";
 
-let baseUrl = import.meta.env.VITE_BASE_API_URL + "/user"
+let baseUrl = import.meta.env.VITE_BASE_API_URL || "http://localhost:5012/api/users";
 
-
+// REGISTER USER
 export const requestUserRegister = async (data) => {
-    try {
-        let result = await axios.post(`${baseUrl}/register`, data)
-        return result
-    } catch (err) {
-        throw err
-    }
-}
+    return axios.post(`${baseUrl}/register`, data);
+};
 
-export const requestUserEmailOtpVerification = async (data) => {
-    try {
-        let result = await axios.post(`${baseUrl}/verify-otp`, data)
-        return result
-    } catch (err) {
-        throw err
-    }
-}
+// VERIFY EMAIL OTP
+export const requestUserEmailOtpVerification = async (email, otp) => {
+    return axios.post(`${baseUrl}/verify-otp`, {
+        email,
+        userOtp: otp?.toString()
+    });
+};
+
+// LOGIN USER
 
 export const requestUserLogin = async (data) => {
     try {
@@ -29,12 +25,12 @@ export const requestUserLogin = async (data) => {
         throw err
     }
 }
-
+// FETCH USER PROFILE
 export const requestUserProfile = async (token) => {
     try {
         let result = await axios({
             method: "GET",
-            url: `${baseUrl}/fetch-urser-profile`,
+            url: `${baseUrl}/fetch-user-profile`,
             headers: {
                 authorization: token
             }
