be6fee94a121b04b28501606e3e10dbae43faeac
komal <komalrangari627@gmail.com>
2025-12-11 19:11:32 +0530

    Routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -11,6 +11,11 @@ import { UserProvider } from './context/userContext.jsx'
 import { MessageProvider } from './context/messageContext.jsx'
 import Message from './components/sections/actions/Message.jsx'
 import UserDashboard from './components/pages/UserDashboard/UserDashboard.jsx'
+import { JobProvider } from './context/jobContext.jsx'
+import DisplayJob from './components/pages/DisplayJob.jsx'
+
+import { CompanyProvider } from "./context/companyContext.jsx";
+import CompanyPublic from "./components/pages/CompanyPublic.jsx";
 
 const App = () => {
 
@@ -19,13 +24,19 @@ const App = () => {
       <UserProvider>
         <MessageProvider>
           <Message />
-          <Router>
-            <Routes>
-              <Route path='/' element={<Home />} />
-              <Route path='/user-login-register' element={<UserLoginRegister />} />
-              <Route path='/user-dashboard' element={<UserDashboard />} />
-            </Routes>
-          </Router>
+          <JobProvider>
+            <CompanyProvider>
+              <Router>
+                <Routes>
+                  <Route path='/' element={<Home />} />
+                  <Route path='/user-login-register' element={<UserLoginRegister />} />
+                  <Route path='/user-dashboard' element={<UserDashboard />} />
+                  <Route path='/job/:jobId' element={<DisplayJob />} />
+                  <Route path='/company/:companyId' element={<CompanyPublic />} />
+                </Routes>
+              </Router>
+            </CompanyProvider>
+          </JobProvider>
         </MessageProvider>
       </UserProvider>
     </>
@@ -33,3 +44,22 @@ const App = () => {
 }
 
 export default App
+
+
+// fetch all jobs -> create page for each job -> display job info and posted by info -> then create a company public page
+
+// new user(Not logedIn Yet)
+// home (Job grid)
+// company public page
+// single job page
+
+// new user(logedIn)
+// apply to job
+// dashboard
+  // create/edit profile
+  // track job application
+
+// company dashboard
+// create jobs
+// edit profile
+// view application
