dda249d7a1a8f0715f656eff4e39eb95426efc78
komal <komalrangari627@gmail.com>
2025-12-11 19:04:41 +0530

    controllers

--- server/controllers/jobController.js
+++ server/controllers/jobController.js
@@ -153,5 +153,27 @@ const getJobData = async (req, res) => {
     res.status(500).json({ message: "Unable to fetch jobs!", error: err });
   }
 };
+//  Get a single job by ID
+ const getSingleJob = async (req, res) => {
+  try {
+    const { jobId } = req.params;
 
-export { createJob, handleJobAction, handleJobApplication, getJobData };
+    const job = await jobModel.findById(jobId).populate(
+      "jobCreatedBy",
+      "companyDetails.name companyDetails.industry"
+    );
+
+    if (!job) {
+      return res.status(404).json({ message: "Job not found" });
+    }
+
+    res.status(200).json(job);
+  } catch (error) {
+    res.status(500).json({
+      message: "Error fetching job",
+      error: error.message,
+    });
+  }
+};
+
+export { createJob, handleJobAction, handleJobApplication, getJobData, getSingleJob };
