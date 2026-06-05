1212f20241425993779a80341c0c4e447d20a071
komal <komalrangari627@gmail.com>
2026-06-05 19:04:09 +0530

    stage file

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,9 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
-export default {
-  build: {
-    chunkSizeWarningLimit: 1000,
-  },
-};
+export default defineConfig({
+  plugins: [react()]
+})
